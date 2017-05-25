77f56803708339acc1de3caa86510f25a097cefe
kumbh <kumbhamsandeep123@gmail.com>
2017-05-26 01:06:59 +0530

    change in the network protocol

--- app/src/main/java/quest/com/quest/NetworkUtils/Constants.java
+++ app/src/main/java/quest/com/quest/NetworkUtils/Constants.java
@@ -6,7 +6,7 @@ package quest.com.quest.NetworkUtils;
 
 public class Constants {
 
-    public static final String APPLICATION_BASE_URL ="http://192.168.0.101/exam-app/api/";
+    public static final String APPLICATION_BASE_URL ="https://192.168.0.101/exam-app/api/";
     public static final String LOGIN_URL ="signin-student";
     public static final String FORGOT_PASSWORD ="";
 
