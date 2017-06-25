d51895866db6b52329843d607337eda34f9bc803
skumbam <skumbam@blackberry.com>
2017-06-25 21:49:58 +0530

    integrate examlist api and adding necessary changes to the code . Changed the DB design accordingly.

--- app/src/main/java/quest/com/quest/Teacher/AnswersRecyclerAdapter.java
+++ app/src/main/java/quest/com/quest/Teacher/AnswersRecyclerAdapter.java
@@ -1,12 +1,19 @@
 package quest.com.quest.Teacher;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+import quest.com.quest.NetworkUtils.Constants;
 import quest.com.quest.R;
 import quest.com.quest.models.ListofExams;
 import quest.com.quest.models.StartExamModel;
@@ -20,10 +27,7 @@ public class AnswersRecyclerAdapter extends RecyclerView.Adapter<AnswersRecycler
     private StartExamModel examsBean;
 
 
-    public AnswersRecyclerAdapter(Context context){
 
-        this.ctx =context;
-    }
 
     public AnswersRecyclerAdapter(Context context , StartExamModel examsBean){
         this.ctx = ctx;
@@ -39,11 +43,96 @@ public class AnswersRecyclerAdapter extends RecyclerView.Adapter<AnswersRecycler
     }
 
     @Override
-    public void onBindViewHolder(HolderView holder, int position) {
+    public void onBindViewHolder(final HolderView holder, int position) {
         if(getExamQuestion(position) != null){
             StartExamModel.QuestionModel examsBean = getExamQuestion(position);
             holder.questionName .setText(position+1 +" . "+ examsBean.getQuestion());
-            holder.answerForQuestion.setText("Answer " + examsBean.getCorrectAnswer());
+            if(!examsBean.isImage().equalsIgnoreCase("no")){
+                Glide.with(holder.questionName.getContext())
+                        .load((Constants.IMAGE_URL +examsBean.isImage()))
+                        .asBitmap()
+                        .into(new SimpleTarget<Bitmap>(100,100) {
+                            @Override
+                            public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                                holder.questionName.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(holder.answerForQuestion.getResources(),resource), null, null );
+                            }
+                        });
+            }
+
+            switch (examsBean.getCorrectAnswer()){
+                case  "option1" :
+                    holder.answerForQuestion.setText("Answer " + examsBean.getOptionA());
+                    if(!examsBean.isHasOption1Image().equalsIgnoreCase("no")){
+                        Glide.with(holder.answerForQuestion.getContext())
+                                .load((Constants.IMAGE_URL +examsBean.isHasOption1Image()))
+                                .asBitmap()
+                                .into(new SimpleTarget<Bitmap>(100,100) {
+                                    @Override
+                                    public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                                        holder.answerForQuestion.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(holder.answerForQuestion.getResources(),resource), null, null );
+                                    }
+                                });
+                    }
+                    break;
+                case  "option2" :
+                    holder.answerForQuestion.setText("Answer " + examsBean.getOptionB());
+                    if(!examsBean.isHasOption1Image().equalsIgnoreCase("no")){
+                        Glide.with(holder.answerForQuestion.getContext())
+                                .load((Constants.IMAGE_URL +examsBean.isHasOption2Image()))
+                                .asBitmap()
+                                .into(new SimpleTarget<Bitmap>(100,100) {
+                                    @Override
+                                    public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                                        holder.answerForQuestion.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(holder.answerForQuestion.getResources(),resource), null, null );
+                                    }
+                                });
+                    }
+                    break;
+                case  "option3" :
+                    holder.answerForQuestion.setText("Answer " + examsBean.getOptionC());
+                    if(!examsBean.isHasOption1Image().equalsIgnoreCase("no")){
+                        Glide.with(holder.answerForQuestion.getContext())
+                                .load((Constants.IMAGE_URL +examsBean.isHasOption3Image()))
+                                .asBitmap()
+                                .into(new SimpleTarget<Bitmap>(100,100) {
+                                    @Override
+                                    public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                                        holder.answerForQuestion.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(holder.answerForQuestion.getResources(),resource), null, null );
+                                    }
+                                });
+                    }
+                    break;
+                case  "option4" :
+                    holder.answerForQuestion.setText("Answer " + examsBean.getOptionD());
+                    if(!examsBean.isHasOption1Image().equalsIgnoreCase("no")){
+                        Glide.with(holder.answerForQuestion.getContext())
+                                .load((Constants.IMAGE_URL +examsBean.isHasOption4Image()))
+                                .asBitmap()
+                                .into(new SimpleTarget<Bitmap>(100,100) {
+                                    @Override
+                                    public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                                        holder.answerForQuestion.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(holder.answerForQuestion.getResources(),resource), null, null );
+                                    }
+                                });
+                    }
+                    break;
+                default:
+                    holder.answerForQuestion.setText("Answer " + examsBean.getOptionA());
+                    if(!examsBean.isHasOption1Image().equalsIgnoreCase("no")){
+                        Glide.with(holder.answerForQuestion.getContext())
+                                .load((Constants.IMAGE_URL +examsBean.isHasOption1Image()))
+                                .asBitmap()
+                                .into(new SimpleTarget<Bitmap>(100,100) {
+                                    @Override
+                                    public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                                        holder.answerForQuestion.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(holder.answerForQuestion.getResources(),resource), null, null );
+                                    }
+                                });
+                    }
+                    break;
+
+            }
+
         }
 
     }
