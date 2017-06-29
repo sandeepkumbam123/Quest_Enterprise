99fcfaf6331353d8ea2ac9d52a7f7eaa58dc4789
skumbam <skumbam@blackberry.com>
2017-06-29 12:21:09 +0530

    integrate examlist api and adding necessary changes to the code . Changed the DB design accordingly.

--- app/src/main/java/quest/com/quest/Adapters/PreviousAnswersRecyclerAdapter.java
+++ app/src/main/java/quest/com/quest/Adapters/PreviousAnswersRecyclerAdapter.java
@@ -1,12 +1,19 @@
 package quest.com.quest.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+import quest.com.quest.NetworkUtils.Constants;
 import quest.com.quest.R;
 import quest.com.quest.Teacher.AnswersRecyclerAdapter;
 import quest.com.quest.models.PreviousExamsListModel;
@@ -41,22 +48,27 @@ public class PreviousAnswersRecyclerAdapter extends RecyclerView.Adapter<Previou
             PreviousExamsListModel.QuestionListBean examsBean = getExamQuestion(position);
             holder.questionName.setText(position+1+ " . "+examsBean.getQuestion());
             holder.answerForQuestion.setText("Student Answer : "+getAnswer(examsBean));
-
+            String imageURL ="";
             if(examsBean.getUser_anser().equalsIgnoreCase(examsBean.getAnswer())){
-               String imageURL ="";
+
                 switch (examsBean.getUser_anser()){
                     case "option1" :
-
+                        imageURL = examsBean.getIs_option1_image();
+                        break;
                     case "option2" :
-
+                        imageURL = examsBean.getIs_option1_image();
+                        break;
                     case "option3" :
-
+                        imageURL = examsBean.getIs_option1_image();
+                        break;
                     case "option4" :
-
+                        imageURL = examsBean.getIs_option1_image();
+                        break;
                     default:
                         imageURL ="";
                         break;
                 }
+
                 holder.answerForQuestion.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.ic_tickcircle,0);
             }else {
                 if(examsBean.getUser_anser().isEmpty() || examsBean.getUser_anser() == null){
